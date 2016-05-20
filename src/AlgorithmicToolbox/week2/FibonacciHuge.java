4dc4bc00503d54178d407fe0c5850b882c29738e
Ryo <shiyanch@gmail.com>
2016-05-20 23:45:34 +0800

    assignments for <Algorithmic Toolbox> week2

--- src/AlgorithmicToolbox/week2/FibonacciHuge.java
+++ src/AlgorithmicToolbox/week2/FibonacciHuge.java
@@ -16,18 +16,44 @@ public class FibonacciHuge {
             first += second;
             second = temp;
 
-            first %= m;
-            second %= m;
+            if(second > m)
+                second %= m;
+            if(first > m)
+                first %= m;
         }
 
-        return first%m;
+        return first;
+    }
+
+    private static long findLoop(long n, long m) {
+        ArrayList<Long> array = new ArrayList<>();
+
+        int flag = -1;
+        array.add(0l);
+        array.add(1l);
+        array.add(1l);
+        for(int i=3;i<=n;i++) {
+            long temp = (array.get(i-2) + array.get(i-1))%m;
+            array.add(temp);
+            if(temp == 1 && array.get(i-1) == 0) {
+                flag = i - 1;
+                break;
+            }
+        }
+
+        if(flag != -1) {
+            int index = (int) (n % flag);
+            return array.get(index);
+        }
+        else
+            return array.get((int)n);
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         long n = scanner.nextLong();
-        long m = scanner.nextLong();
-        System.out.println(getFibonacciHuge(n, m));
+        long m = scanner.nextInt();
+        System.out.println(findLoop(n, m));
     }
 }
 
